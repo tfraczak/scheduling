29c976ab9d27203a5f660609d5b1bcd0ad5e065c
Tim Fraczak <tfraczak@gmail.com>
2023-02-02 19:09:23 -0500

    style changes for the table

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { PATIENTS, THERAPISTS } from '@constants';
 const App = (): JSX.Element => {
   const [schedule, errorMessage] = printSchedule();
   return (
-    <div>
+    <div style={{ padding: '20px' }}>
       <ul style={{ listStyle: 'none' }}>
         <h4 style={{ margin: 0, marginBottom: '5px' }}>Therapists:</h4>
         {THERAPISTS.map((therapist) => <li>{therapist.name}{therapist.primary ? '*' : ''}</li>)}
@@ -15,16 +15,24 @@ const App = (): JSX.Element => {
         <h4 style={{ margin: 0, marginBottom: '5px' }}>Patients:</h4>
         {PATIENTS.map((patient) => <li>{patient.name}</li>)}
       </ul>
-      <ul style={{ listStyle: 'none' }}>
-        <h4 style={{ color: 'red', fontWeight: 700 }}>{errorMessage}</h4>
-        {schedule.map((assignment) => (
-          <li style={{ display: 'flex' }}>
-            <p style={{ margin: 0, marginRight: '5px', marginBottom: '3px' }}>Time: <span style={{ fontWeight: 700 }}>{assignment.time}</span>,</p>
-            <p style={{ margin: 0, marginRight: '5px', marginBottom: '3px' }}>Therapist: <span style={{ fontWeight: 700 }}>{assignment.therapist}</span>,</p>
-            <p style={{ margin: 0 }}>Patient: <span style={{ fontWeight: 700 }}>{assignment.patient}</span></p>
-          </li>
+      <table style={{ boxSizing: 'border-box', padding: 0, listStyle: 'none', border: '2px solid #ddd' }}>
+        {errorMessage ? (
+          <h4 style={{ color: 'red', fontWeight: 700 }}>{errorMessage}</h4>
+        ) : null}
+        {schedule.map((assignment, idx) => (
+          <tr style={{ width: '100%', display: 'flex', backgroundColor: idx % 2 === 0 ? 'white' : '#eee' }}>
+            <td style={{ width: '130px', margin: 0, marginRight: '5px', marginBottom: '3px' }}>
+              Time: <span style={{ fontWeight: 700 }}>{assignment.time}</span>
+            </td>
+            <td style={{ width: '130px', margin: 0, marginRight: '5px', marginBottom: '3px' }}>
+              Therapist: <span style={{ fontWeight: 700 }}>{assignment.therapist}</span>
+            </td>
+            <td style={{ width: '130px', margin: 0 }}>
+              Patient: <span style={{ fontWeight: 700 }}>{assignment.patient}</span>
+            </td>
+          </tr>
         ))}
-      </ul>
+      </table>
     </div>
   );
 };
